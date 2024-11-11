5fd43520395e258bc9a19e7015a797413eb729f6
Edmund Sumpena <edsumpena@gmail.com>
2024-11-10 22:26:42 -0500

    Working implementation of closest distance computation to triangle

--- PROGRAMS/utils/icp.py
+++ PROGRAMS/utils/icp.py
@@ -9,15 +9,29 @@ class Matching(Enum):
 
 class IterativeClosestPoint():
     def __init__(self, match_mode: Matching) -> None:
+        # Define the algorithm used to find closest points
         self.match_mode: Matching = match_mode
 
     def __call__(self):
         pass
 
-    def match(self):
-        pass
+    def match(
+        self,
+        pt_cloud_A: NDArray[np.float32],
+        pt_cloud_B: NDArray[np.float32]
+    ):
+        """
+        Finds the closest point pairs given two point clouds
+        """
+        if self.match_mode == Matching.LINEAR:
+            return self._linear_match(pt_cloud_A, pt_cloud_B)
 
-    def _linear_match(self):
+    def _linear_match(
+        self,
+        pt_cloud_A: NDArray[np.float32],
+        pt_cloud_B: NDArray[np.float32]
+    ):
+        for 
         pass
 
     def _kd_match(self):
