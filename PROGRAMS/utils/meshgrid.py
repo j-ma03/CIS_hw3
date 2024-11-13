675fe33922dd9c39c92a7311bdf8976c9f0e6daa
Edmund Sumpena <edsumpena@gmail.com>
2024-11-13 01:56:00 -0500

    Working implementation of fast vectorized linear ICP

--- PROGRAMS/utils/meshgrid.py
+++ PROGRAMS/utils/meshgrid.py
@@ -96,109 +96,6 @@ class Triangle():
 
         return BoundingBox(top_left, bottom_right)
 
-    def closest_distance_to(
-        self,
-        points: NDArray[np.float32]
-    ) -> Tuple[NDArray[np.float32], NDArray[np.float32]]:
-        """
-        Computes the closest distance from each point in an Nx3 matrix
-        to the triangle by solving a constrained least-squares problem.
-        Returns an N-dimensional vector of the closest distance to 
-        the triangle and an Nx3 matrix of the closest point on the triangle.
-        """
-
-        # Input should be an Nx3 matrix of points
-        if len(points.shape) != 2 or points.shape[1] != 3:
-            raise ValueError('Points should be an Nx3 matrix!')
-
-        # Construct 3x3 matrix of vertices in Barycentric form
-        # Approach based on Dr. Taylor's slides on finding closest points:
-        # https://ciis.lcsr.jhu.edu/lib/exe/fetch.php?media=courses:455-655:lectures:finding_point-pairs.pdf
-        A = np.array([
-            self.v1,
-            self.v2,
-            self.v3
-        ]).T
-
-        # Add an additional row of ones to system to enforce λ + μ + v = 0
-        A = np.concatenate([A, np.ones((1, A.shape[1]))], axis=0)
-        c_points = np.concatenate([points, np.ones((points.shape[0], 1))], axis=1).T
-
-        # Solve the least squares problem to get λ, μ, v 
-        b = np.linalg.lstsq(A, c_points, rcond=None)[0]
-
-        # Find the point projected onto the plane of the triangle
-        closest_pt = (A @ b).T[:,:3]
-
-        # Find point that is outside the λ, μ, v constraints
-        λ_constraint = b[0] < 0
-        μ_constraint = b[1] < 0
-        v_constraint = b[2] < 0
-
-        # """
-        # Handle the boundary cases where λ < 0
-        if λ_constraint.any():
-            closest_pt[λ_constraint] = self._project_on_seg(
-                closest_pt[λ_constraint].reshape(-1, 3), self.v2, self.v3
-            )
-
-        # Handle the boundary cases where μ < 0
-        if μ_constraint.any():
-            closest_pt[μ_constraint] = self._project_on_seg(
-                closest_pt[μ_constraint].reshape(-1, 3),self.v3, self.v1
-            )
-        
-        # Handle the boundary cases where v < 0
-        if v_constraint.any():
-            closest_pt[v_constraint] = self._project_on_seg(
-                closest_pt[v_constraint].reshape(-1, 3), self.v1, self.v2
-            )
-        # """
-
-        # Compute closest distance as magnitude of the difference vector
-        closest_dist = np.linalg.norm(closest_pt - points, ord=2, axis=1)
-
-        return closest_dist, closest_pt
-
-    def _project_on_seg(
-        self,
-        c: NDArray[np.float32],
-        p: NDArray[np.float32],
-        q: NDArray[np.float32]
-    ) -> NDArray[np.float32]:
-        """
-        Given an set of points c as an Nx3 matrix, find the projection
-        of the closest point onto the line segment pq, where p and q are
-        3D vectors of (x, y, z) coordinates.
-        """
-
-        # c should be an Nx3 matrix of points
-        if len(c.shape) != 2 or c.shape[1] != 3:
-            raise ValueError('c should be an Nx3 matrix!')
-        
-        # p should be a 3D vector
-        if len(p.shape) != 1 or p.shape[0] != 3:
-            raise ValueError(f'p should be a 3D vector of (x, y, z) coordinates!')
-        
-        # q should be a 3D vector
-        if len(q.shape) != 1 or q.shape[0] != 3:
-            raise ValueError(f'q should be a 3D vector of (x, y, z) coordinates!')
-
-        # Compute the degree to which segment pc aligns with pc*,
-        # where c* are the points c projected onto pq
-        λ = c.reshape(-1, 3) - p.reshape(1, 3)
-        λ = λ @ (q - p).reshape(3, 1)
-
-        # Normalize by the degree which the segment pq aligns with pq
-        λ /= np.dot(q - p, q - p)
-        λ = np.maximum(0.0, np.minimum(λ, 1.0))
-
-        # Compute c*, the projection of c onto pq
-        c_star = p + λ * (q - p)
-
-        return c_star
-    
-
     def closest_distance_to(self, points: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         """
         Computes the closest distance from each point in an Nx3 matrix
@@ -223,8 +120,6 @@ class Triangle():
 
         # Solve the least squares problem and get Nx3 matrix of v, λ, μ
         b = np.linalg.lstsq(A, c_points, rcond=None)[0]
-        # print(b.shape)
-        # print((1. - b[0] - b[1]).reshape(-1, 1).shape)
         b = np.concatenate([(1. - b[0] - b[1]).reshape(1, -1), b], axis=0)
 
         # Construct 3x3 matrix of vertices
@@ -264,6 +159,44 @@ class Triangle():
         distances = np.linalg.norm(closest_pt - points, axis=1)
 
         return distances, closest_pt
+
+    def _project_on_seg(
+        self,
+        c: NDArray[np.float32],
+        p: NDArray[np.float32],
+        q: NDArray[np.float32]
+    ) -> NDArray[np.float32]:
+        """
+        Given an set of points c as an Nx3 matrix, find the projection
+        of the closest point onto the line segment pq, where p and q are
+        3D vectors of (x, y, z) coordinates.
+        """
+
+        # c should be an Nx3 matrix of points
+        if len(c.shape) != 2 or c.shape[1] != 3:
+            raise ValueError('c should be an Nx3 matrix!')
+        
+        # p should be a 3D vector
+        if len(p.shape) != 1 or p.shape[0] != 3:
+            raise ValueError(f'p should be a 3D vector of (x, y, z) coordinates!')
+        
+        # q should be a 3D vector
+        if len(q.shape) != 1 or q.shape[0] != 3:
+            raise ValueError(f'q should be a 3D vector of (x, y, z) coordinates!')
+
+        # Compute the degree to which segment pc aligns with pc*,
+        # where c* are the points c projected onto pq
+        λ = c.reshape(-1, 3) - p.reshape(1, 3)
+        λ = λ @ (q - p).reshape(3, 1)
+
+        # Normalize by the degree which the segment pq aligns with pq
+        λ /= np.dot(q - p, q - p)
+        λ = np.maximum(0.0, np.minimum(λ, 1.0))
+
+        # Compute c*, the projection of c onto pq
+        c_star = p + λ * (q - p)
+
+        return c_star
     
     def __repr__(self) -> str:
         """
