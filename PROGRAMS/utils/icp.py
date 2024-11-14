9f97d1a192f5ed2e6cb98429ed34e72a04437936
Edmund Sumpena <edsumpena@gmail.com>
2024-11-13 22:21:28 -0500

    Finished implementation of a simple Octree search

--- PROGRAMS/utils/icp.py
+++ PROGRAMS/utils/icp.py
@@ -35,17 +35,29 @@ class IterativeClosestPoint():
         to a triangle meshgrid.
         """
 
+        # Point cloud should be an Nx3 matrix of (x, y, z) coordinates
+        if len(pt_cloud.shape) != 2 or pt_cloud.shape[1] != 3:
+            raise ValueError('Point cloud should be an Nx3 matrix containing 3D coordinates!')
+
+        # Input meshgrid should be a Meshgrid object
         if not isinstance(meshgrid, Meshgrid):
             raise Exception(f'Expected input meshgrid should be of type Meshgrid but got \'{meshgrid.__class__.__name__}\'!')
 
+        ####
+        ## Search algorithms to find closest points
+        ####
+
+        # Performs a simple linear search for closest points
         if self.match_mode == Matching.SIMPLE_LINEAR:
             return self._simple_linear_match(pt_cloud, meshgrid)
         
+        # Performs a faster vectorized linear search for closest points
         elif self.match_mode == Matching.VECTORIZED_LINEAR:
             return self._vectorized_linear_match(pt_cloud, meshgrid)
         
+        # Performs a simple iterative search for closest points using Octrees
         elif self.match_mode == Matching.SIMPLE_OCTREE:
-            return self._octree_match(pt_cloud, meshgrid)
+            return self._simple_octree_match(pt_cloud, meshgrid)
 
     def _simple_linear_match(
         self,
@@ -53,9 +65,9 @@ class IterativeClosestPoint():
         meshgrid: Meshgrid
     ) -> Tuple[NDArray[np.float32], NDArray[np.float32]]:
         """
-        Implementation of a simple linear search ICP algorithm containing 
-        loops over all data points and Triangles to find the closest point 
-        and distance to the meshgrid.
+        Implementation of a simple linear search for the closest point 
+        containing loops over all data points and Triangles. Finds the 
+        closest point and distance to the meshgrid.
         """
 
         # Populate a matrix of closest distances to the meshgrid
@@ -95,10 +107,10 @@ class IterativeClosestPoint():
         meshgrid: Meshgrid
     ) -> Tuple[NDArray[np.float32], NDArray[np.float32]]:
         """
-        Implementation of a fast vectorized linear search ICP algorithm
+        Implementation of a fast vectorized linear search for the closest point
         containing a only single loop over all Triangles in the meshgrid.
-        Closest point and distance for all data points are updated at once 
-        for each Triangle.
+        Closest point and distance to the meshgrid for all data points are updated
+        at once for each Triangle.
         """
 
         # Populate a matrix of closest distances to the meshgrid
@@ -145,78 +157,86 @@ class IterativeClosestPoint():
 
         return closest_pt, min_dist
     
-    def _octree_match(
+    def _simple_octree_match(
         self,
         pt_cloud: NDArray[np.float32],
-        meshgrid: Meshgrid,
-        tree: Octree = None,
-        closest_pt: NDArray[np.float32] = None,
-        min_dist: NDArray[np.float32] = None
+        meshgrid: Meshgrid
     ) -> Tuple[NDArray[np.float32], NDArray[np.float32]]:
         """
-        Performs an ICP match using an octree with iterative point processing.
-        Each point iteratively checks nodes within the octree to locate the closest
-        point on the meshgrid.
+        Implementation of a simple iterative Octree search to find the
+        closest point using a loop over all data points and elements of 
+        the tree. Find the closest point and distance to the meshgrid for
+        all data points.
         """
 
-        if closest_pt is None:
-            # Populate a matrix of closest points on the meshgrid
-            closest_pt = np.zeros_like(pt_cloud)
-            
-        if min_dist is None:
-            # Populate a matrix of closest distances to the meshgrid
-            min_dist = np.empty(pt_cloud.shape[0])
-            min_dist.fill(np.inf)
+        # Populate a matrix of closest points on the meshgrid
+        closest_pt = np.zeros_like(pt_cloud)
+        
+        # Populate a matrix of closest distances to the meshgrid
+        min_dist = np.empty(pt_cloud.shape[0])
+        min_dist.fill(np.inf)
 
-        if tree is None:
-            # Create a new tree containing triangles from the meshgrid
-            tree = Octree(meshgrid.triangles)
+        # Create a new tree containing triangles from the meshgrid
+        tree = Octree(meshgrid.triangles)
 
+        # Check if tree is empty
         if tree.num_elements == 0:
             return closest_pt, min_dist
+        
+        # Search Octree for every point
+        for i, pt in enumerate(pt_cloud):
+            closest_pt[i], min_dist[i] = self._search_octree(
+                pt, tree, closest_pt[i], min_dist[i]
+            )
 
-        # Iterate through all the points and subtrees
-        if tree.have_subtrees:
-            for i, point in enumerate(pt_cloud):
-                for subtree in tree:
-                    # Extract the bounding box of the subtree
-                    box = subtree.box()
+        return closest_pt, min_dist
+    
+    def _search_octree(
+        self,
+        point: NDArray[np.float32],
+        tree: Octree,
+        closest_pt: NDArray[np.float32],
+        min_dist: float
+    ) -> Tuple[NDArray[np.float32], NDArray[np.float32]]:
+        """
+        Helping function that recursively searches an Octree to
+        find the closest point and distance to the meshgrid, which
+        are stored as elements of the Octree.
+        """
+        
+        # Point should be a 3D vector
+        if len(point.shape) != 1 or point.shape[0] != 3:
+            raise ValueError(f'Point should be a 3D vector of (x, y, z) coordinates!')
+        
+        # Check if tree is empty
+        if tree.num_elements == 0:
+            return closest_pt, min_dist
+        
+        # Get the bounding box of the tree
+        box = tree.box()
+        box.enlarge(min_dist)
 
-                    # Extend the bounding box by a margin determined by the
-                    # current minimum distance from each point
-                    box.enlarge(min_dist[i])
-
-                    # Check if there are any candidates to consider
-                    if box.contains(point[None,]):
-                        # Find candidates where distance to tree's centroid is less than
-                        # the previously recorded minimum distance to a triangle
-                        if np.linalg.norm(point - subtree.center, ord=2) < min_dist[i]:
-                            # Recursively search the subtree
-                            closest_pt, min_dist = self._octree_match(
-                                pt_cloud, meshgrid, subtree, closest_pt, min_dist
-                            )
-
+        # Check if there are any candidates to consider
+        if not box.contains(point[None,]):
+            return closest_pt, min_dist
+        
         # Iterate through all elements of the subtree if node is a child
         if not tree.have_subtrees:
-            for i, point in enumerate(pt_cloud):
-                for triangle in tree.elements:
-                    # Extract the bounding box of the triangle
-                    box = triangle.box()
+            for triangle in tree.elements:
+                # Compute closest distance on the triangle for all candidates
+                dist, pt = triangle.closest_distance_to(point[None,])
 
-                    # Extend the bounding box by a margin determined by the
-                    # current minimum distance from each point
-                    box.enlarge(min_dist[i])
+                if dist[0] < min_dist:
+                    # Update the closest point and minimum distance
+                    closest_pt = pt[0]
+                    min_dist = dist[0]
 
-                    # Check if there are any candidates to consider
-                    if box.contains(point[None,]):
-                        # Compute closest distance on the triangle for all candidates
-                        dist, pt = triangle.closest_distance_to(point[None,])
+            return closest_pt, min_dist
 
-                        # Find candidates where distance to triangle is less than
-                        # the previously recorded minimum distance
-                        if dist[0] < min_dist[i]:
-                            # Update the closest point and minimum distance
-                            closest_pt[i] = pt[0]
-                            min_dist[i] = dist[0]
+        # Recursively process all subtrees
+        for subtree in tree:
+            closest_pt, min_dist = self._search_octree(
+                point, subtree, closest_pt, min_dist
+            )
 
         return closest_pt, min_dist
